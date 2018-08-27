6687d726fc007618f642aa7098a973773330b51f
John Gaffney <john@jgaffney.com>
2018-08-27 05:50:13 -0700

    first commit

---
+++ gulp-joomla/README.md
@@ -0,0 +1,3 @@
+#Workflows Course
+
+This project is an example of workflow management from the linda.com course "Gulp.js, Git, and Browserify: Web Project Workflows". This is to provide a template to set up the LACERA web workflows.
---
+++ gulp-joomla/components/scripts/main.js
@@ -0,0 +1,12 @@
+console.log('loaded Main');
+
+function foo() {
+	console.log("Foo");
+}
+
+function JCaption(param){
+	console.log('The JCaption feature is not utilized in this template: "' + param + '" discarded.');
+}
+function popover(param){
+	console.log('The "popover" feature is not utilized in this template: "' + param + '" discarded.');
+}
